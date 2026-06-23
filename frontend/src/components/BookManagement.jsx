d81026934098b16bb2f91d9c2a0d7fc4a01192cd
Harshit Rai <149526938+Harshitrai2005@users.noreply.github.com>
2026-06-23 23:36:46 +0530

    changes fixed for add book

--- frontend/src/components/BookManagement.jsx
+++ frontend/src/components/BookManagement.jsx
@@ -80,14 +80,19 @@ const BookManagement = () => {
             placeholder="Search books by title"
             className="border border-black px-4 py-2 rounded-md focus:outline-none"
           />
+         
+        {user?.role === "Admin" && (
           <button
-           onClick={() => {dispatch(resetBookSlice());   
-                           dispatch(toggleAddBookPopup());}}
+            onClick={() => {
+              dispatch(resetBookSlice());   
+              dispatch(toggleAddBookPopup());
+            }}
             className="bg-black text-white px-4 py-2 rounded-md flex items-center space-x-2"
           >
             <BookA className="w-5 h-5" />
             <span>Add Book</span>
           </button>
+        )}
         </div>
       </div>
 
